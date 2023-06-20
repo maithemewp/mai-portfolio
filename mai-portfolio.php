0056a3b9bee2329d397d4ff7f917bd5e76326119
JiveDig <jivetheuniversal@gmail.com>
2023-06-20 16:07:09 -0400

    1.1.1 fix updater object

--- mai-portfolio.php
+++ mai-portfolio.php
@@ -4,7 +4,7 @@
  * Plugin Name:     Mai Portfolio
  * Plugin URI:      https://bizbudding.com/mai-design-pack/
  * Description:     A versatile and lightweight portfolio plugin for Mai Theme.
- * Version:         1.1.0
+ * Version:         1.1.1
  *
  * Author:          BizBudding
  * Author URI:      https://bizbudding.com
@@ -92,7 +92,7 @@ final class Mai_Portfolio_Plugin {
 	private function setup_constants() {
 		// Plugin version.
 		if ( ! defined( 'MAI_PORTFOLIO_VERSION' ) ) {
-			define( 'MAI_PORTFOLIO_VERSION', '1.1.0' );
+			define( 'MAI_PORTFOLIO_VERSION', '1.1.1' );
 		}
 
 		// Plugin Folder Path.
@@ -169,7 +169,7 @@ final class Mai_Portfolio_Plugin {
 			return;
 		}
 
-		PucFactory::buildUpdateChecker( 'https://github.com/maithemewp/mai-portfolio/', __FILE__, 'mai-portfolio' );
+		$updater = PucFactory::buildUpdateChecker( 'https://github.com/maithemewp/mai-portfolio/', __FILE__, 'mai-portfolio' );
 
 		// Maybe set github api token.
 		if ( defined( 'MAI_GITHUB_API_TOKEN' ) ) {
